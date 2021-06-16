1f8fb2f5b91f83cd20dc74332ebdeee06197b1b2
sanjeevsiva17 <sanjeevsiva17@gmail.com>
2021-06-16 22:09:16 +0530

    added example

--- example/main.go
+++ example/main.go
@@ -3,15 +3,15 @@ package main
 import (
 	"net/http"
 
-	"github.com/sanjeevsiva17/route69"
+	"github.com/sanjeevsiva17/elonmux"
 )
 
 func main() {
-	r := route69.NewRouter()
+	r := elonmux.NewRouter()
 
 	r.GET("/name", func(r *http.Request) (statusCode int, data map[string]interface{}) {
 		return 200, map[string]interface{}{
-			"name": "route69",
+			"name": "elonmux",
 		}
 	})
 
